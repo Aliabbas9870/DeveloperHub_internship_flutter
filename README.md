e1fa16f185b343215b4a638e9e947e9679dbcd6f
Aliabbas9870 <aliab7357@gmail.com>
2025-04-15 11:46:33 +0500

    task1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# DeveloperHub_internship_flutter" 
+Internships all code task upload int this this folder
---
+++ Task1/task1/lib/views/auth/login_auth.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginAuth extends StatelessWidget {
+  const LoginAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
